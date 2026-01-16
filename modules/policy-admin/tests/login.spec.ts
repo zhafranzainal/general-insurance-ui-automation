b8f5409b8220388f7ba0b8343cfb97d4defa5078
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2026-01-16 15:57:21 +0800

    Update URL assertion to use base URL

--- modules/policy-admin/tests/login.spec.ts
+++ modules/policy-admin/tests/login.spec.ts
@@ -3,10 +3,11 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 test('Login to InsureMO Sandbox', async ({ page }) => {
-    await page.goto('/');
+    await page.goto(process.env.BASE_URL || '');
+
     await page.fill('input[formcontrolname="username"]', process.env.GIMO_USER || '');
     await page.fill('input[formcontrolname="password"]', process.env.GIMO_PASS || '');
     await page.click('button:has-text("Login")');
 
-    await expect(page).toHaveURL(/dashboard/i);
+    await expect(page).toHaveURL(process.env.BASE_URL + '#/');
 });
