51d142cd69703bacbc7bfca37fa6feb27357533f
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2026-01-16 15:54:32 +0800

    Create login.spec.ts

---
+++ modules/policy-admin/tests/login.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+test('Login to InsureMO Sandbox', async ({ page }) => {
+    await page.goto('/');
+    await page.fill('input[formcontrolname="username"]', process.env.GIMO_USER || '');
+    await page.fill('input[formcontrolname="password"]', process.env.GIMO_PASS || '');
+    await page.click('button:has-text("Login")');
+
+    await expect(page).toHaveURL(/dashboard/i);
+});
