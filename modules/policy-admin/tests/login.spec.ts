e52394c325db7617bf1d8916161eef0b485ad09a
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2026-01-17 19:45:11 +0800

    Update login test to use login flow

--- modules/policy-admin/tests/login.spec.ts
+++ modules/policy-admin/tests/login.spec.ts
@@ -1,14 +1,11 @@
 import { test, expect } from '../../../shared/tests/base.test.js';
 import * as dotenv from 'dotenv';
-import { LoginPage } from '../../../shared/pages/login.page.js';
+import { LoginFlow } from '../../../shared/flows/login-flow.js';
 
 dotenv.config();
 
 test('Login to InsureMO Sandbox', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-
-    await loginPage.goto();
-    await loginPage.login(process.env.GIMO_USER || '', process.env.GIMO_PASS || '');
-
+    const loginFlow = new LoginFlow(page);
+    await loginFlow.loginAs(process.env.GIMO_USER || '', process.env.GIMO_PASS || '');
     await expect(page).toHaveURL(process.env.BASE_URL + '#/');
 });
